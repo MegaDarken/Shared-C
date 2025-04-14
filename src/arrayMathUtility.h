6230d7caad60187212be508e0596c06c7ad15e0b
MegaDarken <joe.swales@googlemail.com>
2025-04-15 00:10:32 +0100

    arrayMathUtility _array

--- src/arrayMathUtility.h
+++ src/arrayMathUtility.h
@@ -4,28 +4,32 @@
 #include "mathUtility.h"
 
 #define arrayMathUtility_max(array, count) \
-    ({ __typeof__ (array[0]) _output = typeMin(array[0]); \
+    ({ __typeof__ (array) _array = (array); \
+    __typeof__ (array[0]) _output = typeMin(array[0]); \
     for ( size_t _index = 0; _index < count; _index++ ) { \
         _output = max(_output, _array[_index]); \
     } \
     _output; })
 
 #define arrayMathUtility_min(array, count) \
-    ({ __typeof__ (array[0]) _output = typeMax(array[0]); \
+    ({ __typeof__ (array) _array = (array); \
+    __typeof__ (array[0]) _output = typeMax(array[0]); \
     for ( size_t _index = 0; _index < count; _index++ ) { \
         _output = min(_output, _array[_index]); \
     } \
     _output; })
 
 #define arrayMathUtility_absMax(array, count) \
-    ({ __typeof__ (array[0]) _output = typeMin(array[0]); \
+    ({ __typeof__ (array) _array = (array); \
+    __typeof__ (array[0]) _output = typeMin(array[0]); \
     for ( size_t _index = 0; _index < count; _index++ ) { \
         _output = absMax(_output, _array[_index]); \
     } \
     _output; })
 
 #define arrayMathUtility_absMin(array, count) \
-    ({ __typeof__ (array[0]) _output = typeMax(array[0]); \
+    ({ __typeof__ (array) _array = (array); \
+    __typeof__ (array[0]) _output = typeMax(array[0]); \
     for ( size_t _index = 0; _index < count; _index++ ) { \
         _output = absMin(_output, _array[_index]); \
     } \
