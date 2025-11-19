5a3c8cb8caf25ce9dd7b2ad654268aaa9751dfae
MegaDarken <joe.swales@googlemail.com>
2025-11-19 11:02:19 +0000

    charMenu_choiceUpper & Lower

--- src/charMenu.c
+++ src/charMenu.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 void charMenu_present(const int optionCount, char options[optionCount], char **messages)
 {
@@ -28,6 +29,34 @@ int charMenu_choice(const int optionCount, char options[optionCount])
         }
     }
     
+    return -1;
+}
+
+int charMenu_choiceUpper(const int optionCount, char options[optionCount])
+{
+    unsigned char input = toupper(rawRead());
+
+    for (int i = 0; i < optionCount; i++)
+    {
+        if (options[i] == input)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int charMenu_choiceLower(const int optionCount, char options[optionCount])
+{
+    unsigned char input = tolower(rawRead());
+
+    for (int i = 0; i < optionCount; i++)
+    {
+        if (options[i] == input)
+        {
+            return i;
+        }
+    }
     return -1;
 }
 
--- src/charMenu.h
+++ src/charMenu.h
@@ -2,7 +2,12 @@
 #define CHAR_MENU_
 
 void charMenu_present(const int optionCount, char options[optionCount], char **messages);
+
 int charMenu_choice(const int optionCount, char options[optionCount]);
+
+int charMenu_choiceUpper(const int optionCount, char options[optionCount]);
+int charMenu_choiceLower(const int optionCount, char options[optionCount]);
+
 int charMenu_presentChoice(const int optionCount, char options[optionCount], char **messages);
 
 #endif //CHAR_MENU_
