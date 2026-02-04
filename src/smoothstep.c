e62bc1741ab04649a9c18cf17ed8a65564f6f5f8
MegaDarken <joe.swales@googlemail.com>
2026-02-04 14:41:57 +0000

    create smoothstep

---
+++ src/smoothstep.c
@@ -0,0 +1,43 @@
+/*
+Copyright (C) 2026 Joseph Swales
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "smoothstep.h"
+
+#include "clamp.h"
+
+float smoothstep(const float value)
+{
+    return value * value * (3.0f - 2.0f * value);
+}
+
+float clampedSmoothstep(const float edge0, const float edge1, float value)
+{
+    value = clamp((value - edge0) / (edge1 - edge0));
+
+    return smoothstep(value);
+}
+
+float smootherstep(const float value)
+{
+    return value * value * value * (value * (6.0f * value - 15.0f) + 10.0f);
+}
+
+float clampedSmootherstep(const float edge0, const float edge1, float value)
+{
+    value = clamp((value - edge0) / (edge1 - edge0));
+
+    return smootherstep(value);
+}
