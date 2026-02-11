bc3efb6929dd29f5b7edc7fc79077d64ebea510a
MegaDarken <joe.swales@googlemail.com>
2026-02-11 17:51:21 +0000

    create floatColor

---
+++ src/floatColor.c
@@ -0,0 +1,27 @@
+/*
+Copyright (C) 2026 Joseph Swales
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "floatColor.h"
+
+float floatColor_fromByte(const unsigned char value, const float lower, const float upper)
+{
+    return ((float)value / 255) * (upper - lower) + lower;
+}
+
+unsigned char floatColor_toByte(const float value, const float lower, const float upper)
+{
+    return (value - lower) * 255 / (upper - lower);
+}
