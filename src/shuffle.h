2dff6e64d2f8819fc03bacb1f60dc2c447faa231
MegaDarken <joe.swales@googlemail.com>
2026-04-16 17:14:08 +0100

    add shuffle, modified from Temporate

---
+++ src/shuffle.h
@@ -0,0 +1,35 @@
+#ifndef SHUFFLE_
+#define SHUFFLE_
+
+#include <stdlib.h>
+
+#include "swapping.h"
+
+#define shuffle_helper(_1, _2, _3, NAME, ...) NAME
+
+#define shuffle_known(array, count) \
+    do { \
+        __typeof__ (count) _count = (count);\
+        if (_count <= 1) break;\
+        __typeof__ (array) _array = (array);\
+        for ( __typeof__ (count) _i = _count - 1; _i > 1; _i-- ) { \
+            __typeof__ (count) _rand = (rand() % _i); \
+            swap(&(_array[_i]), &(_array[_rand])); \
+        } \
+    } while(0)
+
+#define shuffle_size(array, count, elementSize) \
+    do { \
+        __typeof__ (count) _count = (count);\
+        if (_count <= 1) break;\
+        char *_array = (char *)(array);\
+        __typeof__ (elementSize) _elementSize = (elementSize);\
+        for ( __typeof__ (_count) _i = _count - 1; _i > 1; _i-- ) { \
+            __typeof__ (count) _rand = (rand() % _i); \
+            swap(&_array[_i * _elementSize], &_array[_rand * _elementSize], _elementSize); \
+        } \
+    } while(0)
+
+#define shuffle(...) shuffle_helper(__VA_ARGS__, shuffle_size, shuffle_known)(__VA_ARGS__)
+
+#endif //SHUFFLE_
